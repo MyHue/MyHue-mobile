08f0616b88340b03ecdc4e4e280ed8c49b3dc78c
Henry Paulino <henrypl360@gmail.com>
2017-09-09 17:29:41 -0400

    style tab bar

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,7 +3,9 @@ import { View, Text } from 'react-native'
 import ScrollableTabView, { DefaultTabBar } from 'react-native-scrollable-tab-view'
 import Lights from '../../components/Lights'
 import Profile from '../../components/Profile'
+import Settings from '../../components/Settings'
 import TabBar from './tabBar'
+import { FONT, DARK_GRAY, FONT_COLOR } from '../../ui/theme'
 
 import styles from './styles'
 
@@ -13,9 +15,22 @@ class Main extends Component {
       <View style={{ flex: 1 }}>
         <Text style={styles.myHue}>My Hue</Text>
 
-        <ScrollableTabView renderTabBar={() => <DefaultTabBar />} initialPage={0}>
+        <ScrollableTabView
+          renderTabBar={() => (
+            <DefaultTabBar
+              style={styles.tabStyle}
+              underlineStyle={styles.underlineStyle}
+            />
+          )}
+          initialPage={0}
+          tabBarInactiveTextColor={DARK_GRAY}
+          tabBarActiveTextColor={FONT_COLOR}
+          tabBarTextStyle={styles.tabBarTextStyle}
+          tabBarUnderlineStyle={styles.tabBarUnderlineStyle}
+        >
           <Lights tabLabel="Lights" />
           <Profile tabLabel="Profile" />
+          <Settings tabLabel="Settings" />
         </ScrollableTabView>
       </View>
     )
--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -3,11 +3,27 @@ import { FONT, FONT_COLOR } from '../../ui/theme'
 
 export default StyleSheet.create({
   myHue: {
-    marginVertical: 45,
+    marginTop: 45,
+    marginBottom: 40,
     marginLeft: 20,
     fontSize: 27,
     fontFamily: FONT,
     color: FONT_COLOR,
     fontWeight: 'bold',
+  },
+  tabStyle: {
+    borderBottomWidth: 0,
+    height: 29,
+    paddingBottom: 2,
+  },
+  tabBarTextStyle: {
+    fontFamily: FONT,
+    fontSize: 20,
+    letterSpacing: 1,
+  },
+  tabBarUnderlineStyle: {
+    backgroundColor: '#F3A48A',
+    width: 54,
+    left: 41,
   },
 })
